2fc8e615a50cca7a1f82939a6849dc0e14127a1d
PrakGov <55034062+prak112@users.noreply.github.com>
2025-08-14 15:50:06 +0300

    exercise: Build utility for scheduled task to update scraped data

---
+++ app/update_records.py
@@ -0,0 +1,40 @@
+# for notes on data extraction, refer WorldRecords.ipynb
+
+import gazpacho
+import json
+import os
+from swimclub import DataFile
+
+# Constants
+URL = "https://en.wikipedia.org/wiki/List_of_world_records_in_swimming"
+TABLES = (0, 1, 3, 4)
+COURSES = ("LC Men", "LC Women", "SC Men", "SC Women")
+JSONDATA = DataFile.JSONDATA.value
+WHERE = "/home/hfpy/app/"
+
+# prepare parsed HTML content for data extraction
+soup = gazpacho.Soup.get(URL)
+all_tables = soup.find(tag="table", mode="all")
+
+# record required column data as dictionary-of-dictionaries in data
+data = {}
+for table, course in zip(TABLES, COURSES):
+  rows = all_tables[table].find(tag="tr", mode="all")[1:] # escape header row
+  data[course] = {}
+  for row in rows:
+    columns = row.find(tag="td", mode="all")
+    event = columns[0].text
+    time = columns[1].text
+    if "relay" not in event:
+      data[course][event] = time
+
+# write data to JSON file
+current_dir = os.getcwd()
+if "Z" in current_dir: # local project directory
+  with open(JSONDATA, mode="w") as df:
+    json.dump(obj=data, fp=df)
+  print("Data saved in local project directory")
+else:
+  with open(WHERE + JSONDATA, mode="w") as df:
+    json.dump(obj=data, fp=df)
+  print("Data saved in production environment")
